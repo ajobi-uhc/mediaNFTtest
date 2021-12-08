1bc232254580686a5ce19cb20d19227f372fc992
Arya Jakkli <aryajakkli@engr2-2-80-112-dhcp.int.colorado.edu>
2021-12-07 20:36:13 -0700

    almost finished program

--- programs/something/src/lib.rs
+++ programs/something/src/lib.rs
@@ -1,68 +1,81 @@
 use anchor_lang::prelude::*;
 // use anchor_lang::solana_program::program::invoke_signed;
-use anchor_spl::token::{self, Mint, TokenAccount, Token, SetAuthority, ID as SPL_TOKEN_ID};
-// use metaplex_token_metadata::instruction::{
-//     mint_new_edition_from_master_edition_via_token,
-//     create_master_edition,
-//     create_metadata_accounts,
-// };
+use anchor_spl::token::{self, Mint, TokenAccount, Token, SetAuthority,ID as SPL_TOKEN_ID};
+use anchor_spl :: associated_token:: {AssociatedToken};
+use solana_program::program_option::{COption};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
-/*
-Code to create a mediaNFT locker:
-One instruction :lock_nft that takes in the NFT wallet owner as a pubkey
-and the NFT mint address. 
+declare_id!("5Ff4tvaCetzZmxviCK5HfGb1RKqhJYdcqAozCqG5Wxje");
 
-Using mint address and owner it then performs validations and then rederives the metadata struct of the NFT
-It checks to make sure locked isnt true and then proceeds to generate a PDA based on 
-the currently executing program id and then signs the update_metadata instruction
-
-Q:what seeds do I use for the PDA, and how can I then transfer authority back to another wallet
-after program is done executing
-*/
 #[program]
 pub mod something {
-    use metaplex_token_metadata::utils::{assert_initialized, assert_owned_by, assert_signer};
+    use metaplex_token_metadata::utils::{assert_initialized, assert_owned_by ,assert_signer};
     use metaplex_token_metadata::state::{Metadata,Data};
     use metaplex_token_metadata::instruction::{update_metadata_accounts};
+   
     use super::*;
     
 
-    pub fn initialize(ctx: Context<Initialize>, init_nonce:u8) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, _init_nonce:u8,_program_address:Pubkey) -> ProgramResult {
         let program_pda_account = &mut ctx.accounts.program_pda;
         program_pda_account.signerA = "true".to_string();
         Ok(())
     }
+  
+    pub fn lock(ctx: Context<Lock>, init_nonce:u8,_bump_for_metadata:u8) -> ProgramResult {
 
+         fn assert_keys_equal(key1: Pubkey, key2: Pubkey) -> ProgramResult {
+            if key1 != key2 {
+                Err(ErrorCode::InvalidOwnerForMint.into())
+            } else {
+                Ok(())
+            }
+        }
 
+        let user_mint_account = &ctx.accounts.mint_account;
 
-    pub fn lock(ctx: Context<Lock>, init_nonce:u8) -> ProgramResult {
+        let token_account= &ctx.accounts.token_account;
+        let metadata_account = &ctx.accounts.metadata_account;   
 
-        // let user_mint = &ctx.accounts.mint_account;
+        let owner_wallet = &ctx.accounts.owner_wallet;
         
-        // let owner_wallet = &ctx.accounts.owner_wallet;
-        let mint_metadata = & Metadata::from_account_info(&ctx.accounts.metadata_account)?;
-        // let token_metadata_program = &ctx.accounts.token_metadata_program;
+        let mint_metadata = &mut Metadata::from_account_info(&ctx.accounts.metadata_account)?;
         
         
-        // assert_eq!(token_metadata_program.key,&ID);//token metadata program should be valid
-        // assert_owned_by(mint_metadata_account, ctx.accounts.token_metadata_program.key)?; //metadata accunt must be owned by metadata program
-        // assert_owned_by(&user_mint.to_account_info(),owner_wallet.key)?; //mint authority must be users wallet
-        // assert_signer(&owner_wallet)?;
+        assert_eq!(token_account.amount,1);
+        assert_keys_equal(user_mint_account.key(), token_account.mint)?;
+        assert_keys_equal(token_account.owner, *owner_wallet.key)?;
+        assert_owned_by(metadata_account, &ctx.accounts.token_metadata_program.key)?;
 
-        //On the client side generate a program address using this programs id as a seed
+        if !owner_wallet.to_account_info().is_signer {
+            return Err(ErrorCode::OwnerMustBeSigner.into())
+        }
 
-      
-        //Maybe implement creator wallet check
-        // assert_eq!(mint_metadata.data.creators.,eluuneCreator);
+        msg!("is mutable? {}", mint_metadata.is_mutable);
+        if mint_metadata.is_mutable == false {
+            return Err(ErrorCode :: AlreadyLocked.into())
+        }
+
+         
+
+
+        
+
+        msg!("is mutable? after {}", mint_metadata.is_mutable);
+        // let new_data = Data {
+        //     name: mint_metadata.data.name.clone(),
+        //     symbol: mint_metadata.data.symbol.clone(),
+        //     uri: "https://ifgv27za22gw26hddoplvrj6yp6jn2wprr26oqe3wu6hsoqins3q.arweave.net/QU1dfyDWjW144xueusU-w_yW6s-MdedAm7U8eToIbLc".to_string(),
+        //     seller_fee_basis_points: mint_metadata.data.seller_fee_basis_points,
+        //     creators: mint_metadata.data.creators.clone(),
+        // }; 
 
         let new_data = Data {
-            name: mint_metadata.data.name.clone(),
-            symbol: mint_metadata.data.symbol.clone(),
+            name: "scott  has ovaries!!".to_string(),
+            symbol: "crazy".to_string(),
             uri: "https://ifgv27za22gw26hddoplvrj6yp6jn2wprr26oqe3wu6hsoqins3q.arweave.net/QU1dfyDWjW144xueusU-w_yW6s-MdedAm7U8eToIbLc".to_string(),
-            seller_fee_basis_points: mint_metadata.data.seller_fee_basis_points,
-            creators: mint_metadata.data.creators.clone(),
+            seller_fee_basis_points: 500,
+            creators: None
         }; 
         let metadata_infos = [
             ctx.accounts.metadata_account.clone(),
@@ -70,16 +83,17 @@ pub mod something {
 
         ];
         let update_auth_seeds = [
-            b"new_update_auth".as_ref(),
-            b"Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS".as_ref(),
+            "new_update_auth".as_bytes(),
+            
             &[init_nonce]
         ];
        
 
+
         anchor_lang::solana_program::program::invoke_signed(
             &update_metadata_accounts(
                 *ctx.accounts.token_metadata_program.key, 
-               *ctx.accounts.metadata_account.key, 
+               *ctx.accounts.metadata_account.key,
                * ctx.accounts.program_pda_signer.to_account_info().key,
             None,
             Some(new_data),
@@ -104,7 +118,7 @@ pub mod something {
 //Token account and mint is taken in with constraints as seeds 
 //Creating  ['metaplex', metaplex_program_id, mint_id].
 #[derive(Accounts)]
-#[instruction(my_bump:u8)]
+#[instruction(my_bump:u8,program_address: Pubkey)]
 pub struct Initialize<'info> {
     
 
@@ -112,13 +126,14 @@ pub struct Initialize<'info> {
 #[account( 
     init,
     seeds = [
-    b"new_update_auth".as_ref(),
-    b"Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS".as_ref()],
+    "new_update_auth".as_bytes(), //Mark possible attack vector
+    ],
     bump = my_bump,
     payer = owner_wallet,
     space = 400
 )]  
 pub program_pda: Account<'info,SignerA>,
+
 
 #[account(mut)]
 pub owner_wallet: Signer<'info>,
@@ -130,25 +145,35 @@ pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction(my_bump:u8)]
+#[instruction(my_bump:u8, bump_for_metadata:u8)]
 pub struct Lock<'info> {
+    #[account(address = metaplex_token_metadata::ID)]
     pub token_metadata_program: AccountInfo<'info>,
-    pub mint_account: Account<'info,Mint>,
-    
 
-    #[account(seeds = [b"new_update_auth".as_ref(),b"Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS".as_ref()],bump=my_bump)]
+    pub mint_account: Account<'info,Mint>,
+    pub owner_wallet: Signer<'info>, //any random wallet that is signing
+    pub token_account: Account<'info,TokenAccount>,
+
+    #[account(seeds = ["new_update_auth".as_bytes()],bump=my_bump)]
     pub program_pda_signer: Account<'info,SignerA>,
+
+//    #[account(seeds =[owner_wallet.key.as_ref(),ID.as_ref(),mint_account.key().as_ref()],bump = bump_for_associated)]
+//     pub associated_token_account_for_mint: AccountInfo<'info>,
+
+    
 
     #[account(
         mut, 
-        seeds = ["metaplex".as_bytes(),
-        ID.as_ref(),
-        mint_account.to_account_info().key.as_ref()],
-        bump = my_bump
+        // seeds = ["metaplex".as_bytes(),
+        // token_metadata_program.key.as_ref(),
+        // mint_account.key().as_ref()],
+        // bump = bump_for_metadata
     )]
-    pub metadata_account: AccountInfo<'info>,  
+    pub metadata_account: AccountInfo<'info>,  //possible flag here
 
-    pub owner_wallet: Signer<'info>,
+    
+
+
 
     pub system_program: Program<'info, System>,
    
@@ -157,4 +182,16 @@ pub struct Lock<'info> {
 #[account]
 pub struct SignerA{
     pub signerA: String,
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("Invalid owner for mint")]
+    InvalidOwnerForMint,
+
+    #[msg("Account must be signer")]
+    OwnerMustBeSigner,
+
+    #[msg("You NFT is already locked")]
+    AlreadyLocked
 }
--- tests/something.js
+++ tests/something.js
@@ -1,4 +1,5 @@
 const anchor = require('@project-serum/anchor');
+const { token } = require('@project-serum/anchor/dist/cjs/utils');
 describe('something', () => {
 
   // Configure the client to use the local cluster.
@@ -9,25 +10,44 @@ describe('something', () => {
     const provider = anchor.Provider.env();
     anchor.setProvider(provider);
     const program = anchor.workspace.Something;
-    const mintAccount= new anchor.web3.PublicKey("bwrs7C1FFaFfySXw1HmgM98ordGXPW5AKBP7YfvUZ4U");
-    const metadataAccount = new anchor.web3.PublicKey("H2R9ggkTtLBMDAo3tTiT73x2ZjKKj3G5WPhV1zYmhCce")
+    const mintAccount= new anchor.web3.PublicKey("BHGdnvMcmRmZeTYUcsF6zu7yoKL9VBrxzayxfSVDMTz7");
     const tokenMetadataProgram = new anchor.web3.PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s")
-    
+    const tokenAccount = new anchor.web3.PublicKey("2GSwe1ng1LVPpxao2CGbzTcKjFeJ3BJptmQtKQp7h1pL")
+    let [metadataAccount, bumpForMetadata] = await anchor.web3.PublicKey.findProgramAddress(
+      [anchor.utils.bytes.utf8.encode("metadata"),tokenMetadataProgram.toBuffer(),mintAccount.toBuffer()],
+      tokenMetadataProgram
+
+    )
+      console.log("metadataaccount",metadataAccount)
+    // const metadataAccount = new anchor.web3.PublicKey("GUA7uT7YRTBg2FcQz8opwWxDJuNeZGcVs7Cn3uUMcmrr")
+   
+      console.log('metadataaccount',metadataAccount.toString());
 
     const ownerWallet = anchor.web3.Keypair.fromSecretKey(
       new Uint8Array(
         JSON.parse(require("fs").readFileSync('/Users/aryajakkli/.config/solana/id.json', "utf8"))
       )
     );
-        
+      
+      // const ownerWallet = new anchor.web3.PublicKey("FKviGeU7L1djHrbf21z2ijK2N9KAREagtTjhoc9KQZn6")
+
     let [programPda, myBump] = await anchor.web3.PublicKey.findProgramAddress(
-      [Buffer.from("new_update_auth")],
+      [anchor.utils.bytes.utf8.encode("new_update_auth")],
       program.programId,
     )
+    
 
+    // let [findAssociatedToken, associatedBump] = await anchor.web3.PublicKey.findProgramAddress(
+    //   [anchor],
+    //   program.programId
+    // )
+
+
+    
+//PDA: AGKVw4WA8A84BzGVFZquVuVyrTDWPTXgaJwpVr2CYQfD
       console.log('program id', program.programId.toString())
       console.log('created pda:',programPda.toString())
-      console.log('owner wallet', ownerWallet.publicKey.toString())
+      // console.log('owner wallet', ownerWallet.publicKey.toString())
 
     // console.log(' program pda: ', await program.provider.connection.getAccountInfo(programPda));
     // console.log('ownerWallet', await program.provider.connection.getAccountInfo(ownerWallet));
@@ -53,32 +73,34 @@ describe('something', () => {
     // })
 
 
-    const tx = await program.rpc.initialize(
-      myBump,
+  //   const tx = await program.rpc.initialize(
+  //     myBump,
+  //     programPda.programId,
       
-    {
+  //   {
 
-      accounts:{
-      programPda: programPda,
-      ownerWallet:ownerWallet.publicKey,
-      systemProgram: anchor.web3.SystemProgram.programId,
+  //     accounts:{
+  //     programPda: programPda,
+  //     ownerWallet:ownerWallet.publicKey,
+  //     systemProgram: anchor.web3.SystemProgram.programId,
       
       
       
-    },
-      signers:[ownerWallet]
+  //   },
+  //     signers:[ownerWallet]
   
-  });
-    console.log("Your transaction signature", tx);
+  // });
+  //   console.log("Your transaction signature", tx);
 
-    const newtx = await program.rpc.lock(myBump,{
+    const newtx = await program.rpc.lock(myBump,bumpForMetadata,{
 
       accounts:{
         tokenMetadataProgram:tokenMetadataProgram,
         mintAccount:mintAccount,
+        ownerWallet:ownerWallet.publicKey,
+        tokenAccount:tokenAccount,
         programPdaSigner: programPda,
         metadataAccount:metadataAccount,
-        ownerWallet:provider.wallet.publicKey,
         systemProgram: anchor.web3.SystemProgram.programId,
         
       },
